0899111308c03d2001d4ad68f25aba62957918c9
Samuel Barrem <samuelbarremdesouza@hotmail.com>
2026-07-13 15:09:35 -0300

    Filtrando por nome do autor e tratando autor na encontrado

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,4 +1,4 @@
-import { livros } from "../models/index.js";
+import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
@@ -66,28 +66,47 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const { aditora, titulo } = req.query;
+      const busca = await processaBusca(req.query);
 
-      
-
-      const busca = {};
-
-      if(editora) busca.editora = editora;
-      if(titulo) busca.titulo = { $regex: titulo, $oprtions: "i" };
-
-      const editora = req.query.editora;
-
-      const livrosResultado = await livros.find({
-        "editora": editora,
-        titulo: titulo
-      });
+      if (busca !== null) {
+      const livrosResultado = await livros
+        .find(busca)
+        .populate("autor");
 
       res.status(200).send(livrosResultado);
+      } else {
+        res.status(200).send([]);
+      }
     } catch (erro) {
       next(erro);
     }
   };
 
+}
+
+async function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = parametros;
+
+  let busca = {};
+
+  if (editora) busca.editora = editora;
+  if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
+
+  if (minPaginas || maxPaginas) busca.numeroPaginas = {};
+  if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
+  if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
+
+  if (nomeAutor) {
+    const autor = await autores.findOne({ nome: nomeAutor });
+
+    if (autor !== null) {
+      busca.autor = autor._id;   // corrigido
+    } else {
+      busca = null;
+    }
+  }
+
+  return busca;
 }
 
 export default LivroController;
